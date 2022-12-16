891bb93afcb4fcad6af1ffc19dadf459c8692f6b
Netology <eparis@atlassian.com>
2022-12-16 13:06:58 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 1190;
+        int bonus = 20;
+        int miles = amount / bonus * 1;
+
+        System.out.println("Количество начисленных миль за купленный билет :" + miles);
+
+
+    }
+}
